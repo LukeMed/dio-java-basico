46de09a90948c64bc97b05f20ee0fc71b7d2466f
LukeMed <luke.lam1988@gmail.com>
2024-11-15 17:49:22 -0300

    15/11/2024

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Repositório para armazenar todo o conteúdo do curso de Java Básico
 Exercícios de Java propostos pelo Prof Gleyson
 
-Alterando o conteúdo de um arquivo de forma local
+Alterando o conteúdo de um arquivo de forma local.
+Update 15/11/2024.
+
--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,8 @@
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Dio class! Let's go to start to code!");
+
+    String aluno = "Lucas";
+        System.out.println("Hello, Dio class! Let's go to start to code! " + aluno + " quer começar.");
     }
 }
